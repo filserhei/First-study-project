c2002c3d06104b74a104218625934c820958e35e
Serhei <filserhei@gmail.com>
2021-04-03 23:00:30 +0300

    start

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-const  a = 3;
-console.log(4 * 5 * a);
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов посмотрели?', '');
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt('Один из просмотренных последних фильмов', '');
+const b = prompt('Оцените его', '');
+const c = prompt('Один из просмотренных последних фильмов', '');
+const d = prompt('Оцените его', '');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d; 
+console.log(personalMovieDB);
