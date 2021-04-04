dadef9b7186ffc63f6dcea8245e067b08d543e95
Serhei <filserhei@gmail.com>
2021-04-04 12:57:41 +0300

    with condition

--- script.js
+++ script.js
@@ -8,10 +8,27 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из просмотренных последних фильмов', '');
-const b = prompt('Оцените его', '');
-const c = prompt('Один из просмотренных последних фильмов', '');
-const d = prompt('Оцените его', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d; 
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один из просмотренных последних фильмов', ''),
+        b = prompt('Оцените его', '');
+
+  if (a != null && b != null && a.length > 0 && b.length > 0 && a.length <= 50) {
+  personalMovieDB.movies[a] = b;
+  console.log('done');
+  } else {
+    i--;
+    console.log('wrong');
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ощибка');
+}
+
 console.log(personalMovieDB);
